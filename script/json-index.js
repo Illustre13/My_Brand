4e7d0db579d92562a9f38cc3790f38a83f045ff3
Illustre13 <ndahayosibertin17@gmail.com>
2023-02-24 08:37:24 +0200

    Installed json-server
    Integrated json-server in my project
    
    Able to create my blogs dynamically
    
    Added my new created blog to the recent blog section

---
+++ script/json-index.js
@@ -0,0 +1,49 @@
+// javascript for index.html
+// javascript for index.html
+const container = document.querySelector('.recent_blog');
+//const searchForm = document.querySelector('.search');
+
+const renderPosts = async (term) => {
+    let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';
+    if (term) {
+        uri += `&q=${term}`;
+    }
+    const res = await fetch(uri);
+    const posts = await res.json();
+    let template = '';
+    posts.forEach(post => {
+        template += `
+            <div class="article_holder">
+                <img src="/images/com.jpg" alt="article image" />
+                <div class="ah_001">
+                    <h3><a href="/json_details.html?id=${ post.id }">${post.title}</a></h3>
+                   
+                    <p>${post.body.slice(0, 200)}</p>
+                    <div class="ah_misc">
+                        <div class="hashtags">
+                            <p>#${post.category}</p>
+                            <p>#${post.category}</p>
+                        
+                        </div>
+                        <div class="read_date">
+                            <p><i class="fa-solid fa-clock"></i> 3 min reads</p>
+                            <p><i class="fa-solid fa-calendar"></i> 25 November 2022</p>
+                        </div>
+                       
+                    </div>
+                    <div class="read-more">
+                         <a href="/json_details.html?id=${ post.id }">Read more...</a>
+                    </div>
+                    
+                </div>
+            </div>
+        `
+    })
+    container.innerHTML = template;
+}
+/*
+searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    renderPosts(searchForm.term.value.trim())
+})*/
+window.addEventListener('DOMContentLoaded', () => renderPosts());
---
+++ script/json_create.js
@@ -0,0 +1,19 @@
+// javascript for create.html
+// javascript for create.html
+const form = document.querySelector('form');
+const createPost = async (e) => {
+    e.preventDefault();
+    const doc = {
+        title: form.title.value,
+        category: form.category.value,
+        body: form.body.value,
+        likes: 10
+    }
+    await fetch('http://localhost:3000/posts', {
+        method: 'POST',
+        body: JSON.stringify(doc),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    window.location.replace('/dashboard.html');
+}
+form.addEventListener('submit', createPost);
