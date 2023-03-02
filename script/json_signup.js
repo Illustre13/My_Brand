63abfd3f92b81ab4924e8d8029ef51668fb0240c
Illustre13 <ndahayosibertin17@gmail.com>
2023-03-02 12:26:19 +0200

    Worked on Signup aunthetication
    Able to save user data in the json file after signup

---
+++ script/json_signup.js
@@ -0,0 +1,47 @@
+
+let emailExists = async (email)=>{
+    let uri = 'http://localhost:3000/users?email='
+    uri += `${email}`
+    const res = await fetch(uri)
+    const user = await res.json();
+
+    if(user.length === 0){
+        return false
+    }
+    return true
+}
+
+let error_message = document.getElementById('error_message')
+//let title = document.getElementById('form_title')
+
+const form = document.getElementById('signUpForm');
+const createUser = async (e) => {
+    e.preventDefault();
+    let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    if(form.email.value.match(validRegex)){
+        let emExists = await emailExists(form.email.value)
+        console.log( 'Hey', emExists)
+        if (emExists){
+            error_message.innerText = 'Email already exists, log in'
+        }else{
+
+            const doc = {
+                name: form.uname.value,
+                email: form.email.value,
+                password: form.confirm_password.value,
+                role:"user",
+            }
+            await fetch('http://localhost:3000/users', {
+                method: 'POST',
+                body: JSON.stringify(doc),
+                headers: { 'Content-Type': 'application/json' }
+            });
+            user_details = {name: form.uname.value,email: form.email.value,password: form.password_001.value, role: 'user'}
+            localStorage.setItem('user',JSON.stringify(user_details))
+            window.location.replace('/login_page.html');
+        }
+    }else{
+        error_message.innerText = 'Invalid Email'
+    }
+}
+form.addEventListener('submit', createUser);
--- script/signup.js
+++ script/signup.js
@@ -4,54 +4,74 @@ let emailExists = async (email)=>{
     uri += `${email}`
     const res = await fetch(uri)
     const user = await res.json();
-    if(user[0]){
-        return true
-    }else{
+
+    if(user.length === 0){
         return false
+    }else{
+    return true
     }
+  
 }
 
 let error_message = document.getElementById('error_message')
-let title = document.getElementById('form_title')
+//let title = document.getElementById('form_title')
+let pswd_001 = document.getElementById("password");
+let pswd_002 = document.getElementById("confirm_password");
 
-const form = document.getElementById('create_user');
-const createUser = async (e) => {
-    e.preventDefault();
+const form = document.getElementById('signUpForm');
+
     let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
     if(form.email.value.match(validRegex)){
+      
         if (emailExists(form.email.value)){
-            title.style.marginBottom = '0.5rem'
+           // title.style.marginBottom = '0.5rem'
             error_message.innerText = 'Email already exists, log in'
         }else{
-            const doc = {
-                name: form.name.value,
-                email: form.email.value,
-                password: form.password.value,
-                role:"user",
+            if(pswd_001 !== pswd_002){
+                error_message.innerHTML = 'Password Does not match'
+            }else{
+                if(form.email.value == "ndahayosibertin17@gmail.com"){
+                    const createUser = async (e) => {
+                        let emExists = await emailExists(form.email.value)
+                        console.log( 'Hey', emExists)
+                    e.preventDefault();
+                    const doc = {
+                        name: form.uname.value,
+                        email: form.email.value,
+                        password: form.password.value,
+                        role:"admin",
+                    }
+                    await fetch('http://localhost:3000/users', {
+                        method: 'POST',
+                        body: JSON.stringify(doc),
+                        headers: { 'Content-Type': 'application/json' }
+                    });
+                    window.location.replace('/login_page.html');
+                }
+                form.addEventListener('submit', createUser);
+                }else{
+                    const createUser = async (e) => {
+                        e.preventDefault();
+                    const doc = {
+                        name: form.uname.value,
+                        email: form.email.value,
+                        password: form.password.value,
+                        role:"user",
+                    }
+                    await fetch(`http://localhost:3000/users`, {
+                        method: 'POST',
+                        body: JSON.stringify(doc),
+                        headers: { 'Content-Type': 'application/json' }
+                    });
+                    window.location.replace('/login_page.html');
+                }
+                form.addEventListener('submit', createUser);
+
+                }
+                
             }
-            await fetch('http://localhost:3000/users', {
-                method: 'POST',
-                body: JSON.stringify(doc),
-                headers: { 'Content-Type': 'application/json' }
-            });
-            window.location.replace('/index.html');
+
         }
     }else{
-        title.style.marginBottom = '0.5rem'
-        error_message.innerText = 'Invalid Email'
+        error_message.innerText = 'Use a valid Email'
     }
-}
-form.addEventListener('submit', createUser);
-
-let password_view = document.getElementById("p-viewer");
-let password_input = document.getElementById('password')
-password_view.addEventListener('click',()=>{
-    if (password_input.type === "password") {
-        password_input.type = "text";
-        password_view.innerHTML = '<i class="fa-solid fa-eye-slash"></i>'
-      } else {
-        password_input.type = "password";
-        password_view.innerHTML = '<i class="fa fa-eye" aria-hidden="true"></i>'
-      }
-})
-Footer
