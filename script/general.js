f4e40d011880aba8a33859988419260e6f20e4dc
Ndahayo S Bertin <ndahayosibertin17@gmail.com>
2023-03-09 09:31:31 +0200

    Revert "Added The Switching Tabs to the Admin Dashboard"
    This reverts commit 6f0af0fcda17295ab7feb925a5028f8b47c53f1f.
    
    Reverted to previous commit; Have committed same changes in the develop
    branch

--- script/general.js
+++ script/general.js
@@ -3,24 +3,3 @@ function footer(){
     document.getElementById("FOOTER").innerHTML = "<span> © Copyright 2023 | Powered By<strong><a href='#'> ITH</a></strong>";
     
 }
-
-
-      function openTab(evt, tabName) {
-        var i, tabcontent, tablinks;
-
-        // Get all elements with class="tab" and hide them
-        tabcontent = document.getElementsByClassName("tab");
-        for (i = 0; i < tabcontent.length; i++) {
-          tabcontent[i].style.display = "none";
-        }
-
-        // Get all elements with class="tab-button" and remove the class "active"
-        tablinks = document.getElementsByClassName("tab-button");
-        for (i = 0; i < tablinks.length; i++) {
-          tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-
-        // Show the current tab, and add an "active" class to the button that opened the tab
-        document.getElementById(tabName).style.display = "block";
-        evt.currentTarget.className += " active";
-      }
