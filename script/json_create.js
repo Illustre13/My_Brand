b5af40cff190df09c821b8444d7f3a9af769786f
Ndahayo S Bertin <ndahayosibertin17@gmail.com>
2023-03-27 10:26:09 +0200

    Worked on image upload on cloudinary
    Worked on editing blogs

--- script/json_create.js
+++ script/json_create.js
@@ -7,14 +7,14 @@
                 location.replace('./login_page.html')   
             }
             else{
-              console.log("You don't have enough privilege!!");
+              console.log("Privilege User-- ADMIN!!");
             }
 
          });
 let article_id = new URLSearchParams(window.location.search).get("id");
 var outImage =document.getElementById("imagenFondo");
-var image=new Image();
-var srcData;
+var imageInput = document.getElementById("file-selector");
+let imageStored;
 var test = "Test";
 let article;
 //const date_published = new date();
@@ -27,6 +27,34 @@ if(article_id !== null ){
   
 
 }
+
+//Image Upload on cloudinary
+async function uploadProcess(imageToUpload) {
+
+  const data = new FormData();
+  data.append("file", imageToUpload);
+  data.append("upload_preset", "brand_api_upload");
+  data.append("cloud_name", "dwbvnslii");
+
+  const Http = new XMLHttpRequest();
+  const url = "https://api.cloudinary.com/v1_1/dwbvnslii/image/upload";
+  Http.open("POST", url);
+  Http.send(data);
+
+  Http.onreadystatechange = (e) => {
+      let res = JSON.parse(Http.responseText);
+      imageStored = res.url;
+  }
+}
+
+imageInput.onchange = event => {
+  const file = event.target.files[0];
+  let image = URL.createObjectURL(file);
+
+  outImage.src = image;
+  uploadProcess(file);
+}
+
 async function call_article() {
   let res = await fetch(`https://ith-mybrand-backend.onrender.com/blog/${article_id}`)
   article = await res.json()
@@ -36,72 +64,46 @@ form.body.value = article.content;
 form.imagenFondo.src = article.image
 }
 
-function preview_2(obj)
-{
-	if (FileReader)
-	{
-		var reader = new FileReader();
-		reader.readAsDataURL(obj.files[0]);
-		reader.onload = function (e) {
-//		var image=new Image();
-		image.src=e.target.result;
-        srcData = e.target.result;
-       // console.log(srcData)
-		image.onload = function () {
-			form.imagenFondo.src=image.src;
-		};
-        image.onload = function () {
-            console.log("HI HI HI");
-            console.log(srcData);
-            const form = document.querySelector('form');
-            form.imagenFondo.src = srcData;
-            
-            if(article_id !==null){
-              const createPost = async (e) => {
-                e.preventDefault();
-                const doc = {
-                    title: form.title.value,
-                    category: form.category.value,
-                    content: form.body.value,
-                    image: srcData
-                }
-                await fetch(`https://ith-mybrand-backend.onrender.com/update_blog/${article_id}`, {
-                    method: 'PUT',
-                    body: JSON.stringify(doc),
-                    headers: { 'Content-Type': 'application/json' }
-                });
-                window.location.replace('/dashboard.html');
-            }
-            form.addEventListener('submit', createPost);
-            }
-            else{
-              const createPost = async (e) => {
-                e.preventDefault();
-                const doc = {
-                    title: form.title.value,
-                    category: form.category.value,
-                    content: form.body.value,
-                    image: srcData
-                }
-                await fetch('https://ith-mybrand-backend.onrender.com/create_blog', {
-                    method: 'POST',
-                    body: JSON.stringify(doc),
-                    headers: { 'Content-Type': 'application/json' }
-                });
-                window.location.replace('/dashboard.html');
-            }
-            form.addEventListener('submit', createPost);
-            }
-            
-            
-        }
-		}
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  // const form = document.querySelector('form');
+  //           form.imagenFondo.src = imageStored;
+  if(article_id !==null){
+    const createPost = async (e) => {
+      e.preventDefault();
+      const doc = {
+          title: form.title.value,
+          category: form.category.value,
+          content: form.body.value,
+          image: imageStored
+      }
+      await fetch(`https://ith-mybrand-backend.onrender.com/update_blog/${article_id}`, {
+          method: 'PUT',
+          body: JSON.stringify(doc),
+          headers: { 'Content-Type': 'application/json' }
+      });
+      window.location.replace('/dashboard.html');
+  }
+  form.addEventListener('submit', createPost);
+  }
+  else{
+    const createPost = async (e) => {
+      e.preventDefault();
+      const doc = {
+          title: form.title.value,
+          category: form.category.value,
+          content: form.body.value,
+          image: imageStored
+      }
+      await fetch('https://ith-mybrand-backend.onrender.com/create_blog', {
+          method: 'POST',
+          body: JSON.stringify(doc),
+          headers: { 'Content-Type': 'application/json' }
+      });
+      window.location.replace('/dashboard.html');
+  }
+  form.addEventListener('submit', createPost);
+  }
 
-        
-            
-	}
-	else
-	{
-		    // Not supported
-	}
-}
+  
+})
